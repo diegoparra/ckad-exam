9881e2717063ed84b168615b84581f8287e36f4b
Diego Parra <diego.ferreira@miraeducacao.com.br>
2018-06-15 15:22:10 -0300

    add questions

--- ckad.py
+++ ckad.py
@@ -5,8 +5,8 @@ import os
 basic_conf = [{
     'question': 'Basic configurations\n\n\nHow to create kubectl completion? \n',
     'answer': 'echo "source <(kubectl completion bash)" >> ~/.bashrc',
-    'obs': 'first obs',
-    'link': 'http://google.com \n',
+    'obs': '',
+    'link': '',
     },
     {
     'question': 'How to reload .bashrc ? \n',
@@ -81,7 +81,43 @@ core_concepts = [{
     'answer': '--runtime-config',
     'obs': 'enable: --runtime-config=batch/v2alpha1\n disable: --runtime-config=batch/v1=false\n',
     'link': 'https://kubernetes.io/docs/concepts/overview/kubernetes-api/',
-    }
+    },
+    {
+    'question': 'What are the pods controllers?\n',
+    'answer': 'deployment, statefulset, daemonset',
+    'obs': '',
+    'link': 'https://kubernetes.io/docs/concepts/workloads/pods/pod-overview/',
+    },
+    {
+    'question': 'What are the Container probes types ?\n',
+    'answer': 'ExecAction, TCPSockAction, HTTPGetAction',
+    'obs': '\nExecAction: Executes a specified command inside the Container. The diagnostic is considered successful if the command exits with a status code of 0. \n TCPSocketAction: Performs a TCP check against the Container’s IP address on a specified port. The diagnostic is considered successful if the port is open. \n HTTPGetAction: Performs an HTTP Get request against the Container’s IP address on a specified port and path. The diagnostic is considered successful if the response has a status code greater than or equal to 200 and less than 400. \n',
+    'link': 'https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/',
+    },
+    {
+    'question': 'Each probe has one of three results, what is this results? \n',
+    'answer': 'Success, Failure, Unknown',
+    'obs': 'Success: The Container passed the diagnostic.\n Failure: The Container failed the diagnostic. \n Unknown: The diagnostic failed, so no action should be taken.\n',
+    'link': 'https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/',
+    },
+    {
+    'question': 'The kubelet can optionally perform and react to two kinds of probes on running Containers, what are? \n',
+    'answer': 'livenessProbe, readinessProbe',
+    'obs': '\nlivenessProbe: Indicates whether the Container is running. If the liveness probe fails, the kubelet kills the Container, and the Container is subjected to its restart policy. If a Container does not provide a liveness probe, the default state is Success.\n readinessProbe: Indicates whether the Container is ready to service requests. If the readiness probe fails, the endpoints controller removes the Pod’s IP address from the endpoints of all Services that match the Pod. The default state of readiness before the initial delay is Failure. If a Container does not provide a readiness probe, the default state is Success\n',
+    'link': 'https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/',
+    },
+    {
+    'question': 'What are the  possible values to a restartPolicy ? \n',
+    'answer': 'Always, OnFailure, and Never',
+    'obs': '\nThe default value is Always. restartPolicy applies to all Containers in the Pod. restartPolicy only refers to restarts of the Containers by the kubelet on the same node. Exited Containers that are restarted by the kubelet are restarted with an exponential back-off delay (10s, 20s, 40s …) capped at five minutes, and is reset after ten minutes of successful execution. As discussed in the Pods document, once bound to a node, a Pod will never be rebound to another node\n',
+    'link': 'https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/',
+    },
+    {
+    'question': 'Pod lifetime, What are the Three types of controllers available? \n',
+    'answer': 'Job, ReplicationController, ReplicaSet, or Deployment and DaemonSet \n',
+    'obs': 'Use a Job for Pods that are expected to terminate, for example, batch computations. Jobs are appropriate only for Pods with restartPolicy equal to OnFailure or Never.\n Use a ReplicationController, ReplicaSet, or Deployment for Pods that are not expected to terminate, for example, web servers. ReplicationControllers are appropriate only for Pods with a restartPolicy of Always.\n Use a DaemonSet for Pods that need to run one per machine, because they provide a machine-specific system service.\n',
+    'link': 'https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/',
+    },
 ]
 
 
@@ -168,7 +204,7 @@ def exec_choose(question):
             os.system("clear")
 
     media = (cq / len(questions) * 100)
-    print('sua media foi: {:.2f}'.format(media))
+    print('Your Result: {:.2f}'.format(media))
 
 def main():
     while True:
